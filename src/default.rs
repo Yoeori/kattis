28d0782f56e0a6cfcfa6d9c4c32f42db0050a8aa
Yoeri Otten <yoeri@otten.dev>
2022-01-17 22:31:26 +0100

    Update default files for easier problem solving

---
+++ src/default.rs
@@ -0,0 +1,39 @@
+use std::error::Error;
+use std::io::{self, BufRead, Write};
+
+/// main entry point of program when invoked directly in the terminal
+#[allow(dead_code)]
+fn main() -> Result<(), Box<dyn Error>> {
+    solve(io::stdin().lock(), io::stdout().lock())
+}
+
+fn solve(input: impl BufRead, mut w: impl Write) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_sampleinputs() {
+        for mut file in std::fs::read_dir("input/default")
+            .unwrap()
+            .filter(|f| f.is_ok() && f.as_ref().unwrap().path().extension().unwrap() == "in")
+            .map(|f| f.unwrap().path())
+        {
+            let mut output_writer: Vec<u8> = Vec::new();
+            solve(
+                std::fs::read_to_string(&file).unwrap().as_bytes(),
+                &mut output_writer,
+            )
+            .unwrap();
+            file.set_extension("ans");
+            assert_eq!(
+                std::str::from_utf8(&output_writer).unwrap().trim(),
+                std::fs::read_to_string(&file).unwrap().trim(),
+                "file: {:?}", &file
+            );
+        }
+    }
+}
